b699dc90ab38b8173931fd12979c981ab2bf47c7
dante <dante7867@gmail.com>
2019-02-05 20:17:17 +0100

    function for circle-circle and rectangle-rectangle collision checking added. Additional refactoring

--- untitled-sdl-tank-game/utils/utils.h
+++ untitled-sdl-tank-game/utils/utils.h
@@ -1,7 +1,15 @@
 #pragma once
 #include "figures.h"
 
-int distanceFromPointToLine(Line line, Point point);
+double distanceFromPointToLine(Line line, Point point);
 bool isRoundAndLineIntersecting(Line line, Round round);
 double coutDelta(double a, double b, double c);
 std::tuple<double, double> countSolutions(double a, double b, double c, double delta);
+
+// for Circle-Circle collison check
+double distanceFromPointToPoint(Point firstPoint, Point secondPoint);
+bool areRoundsIntersecting(Round firstRound, Round secondRound);
+
+// for Rentangle-Rentangle collision check
+bool isPointInRectangle(Point point, Rectangle rectangle);
+bool areRectanglesIntersecting(Rectangle firstRectangle, Rectangle secondRectangle);
