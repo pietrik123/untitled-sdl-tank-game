333f7e7a43e22d7f77b07a5b7e6d35f865c45d6d
pietrik123 <piotrmierzwa042@gmail.com>
2019-02-06 21:55:48 +0100

    small corrections

--- untitled-sdl-tank-game/utils/utils.h
+++ untitled-sdl-tank-game/utils/utils.h
@@ -3,13 +3,13 @@
 #include <vector>
 
 double distanceFromPointToLine(Line line, Point point);
-bool isRoundAndLineIntersecting(Line line, Circle circle);
-double getDeleta(double a, double b, double c);
+bool isCircleAndLineIntersecting(Line line, Circle circle);
+double getDelta(double a, double b, double c);
 std::vector<double> countSolutions(double a, double b, double c, double delta);
 
 // for Circle-Circle collison check
 double distanceFromPointToPoint(Point firstPoint, Point secondPoint);
-bool areRoundsIntersecting(Circle firstCircle, Circle secondCircle);
+bool areCirclesIntersecting(Circle firstCircle, Circle secondCircle);
 
 // for Rentangle-Rentangle collision check
 bool isPointInRectangle(Point point, Rectangle rectangle);
