d3f7bf7b7a3d46c4f79fde4d615edab0d6114a87
dante <dante7867@gmail.com>
2019-02-28 19:00:12 +0100

    google tests added

---
+++ googleTest/test.cpp
@@ -0,0 +1,10 @@
+#include "pch.h"
+#include "../untitled-sdl-tank-game/utils/utils.cpp"
+
+TEST(TestCaseName, TestName) 
+{
+	Point point{ 0, 0 };
+	Line line{ 1, 1, 0 };
+	double distance = distanceFromPointToLine(line, point);
+	EXPECT_EQ(0, distance );
+}
