c9788e833b69eb17680c2895d6152558602c4234
dante <dante7867@gmail.com>
2019-02-28 22:22:43 +0100

    tests added

--- googleTest/test.cpp
+++ googleTest/test.cpp
@@ -1,10 +1,17 @@
 #include "pch.h"
 #include "../untitled-sdl-tank-game/utils/utils.cpp"
+#include "math.h"
 
 TEST(UtilsTestSuite, distanceFromPointToLineBasicTest) 
 {
 	Point point{ 0, 0 };
 	Line line{ 1, 1, 0 };
-	double distance = distanceFromPointToLine(line, point);
-	EXPECT_EQ(0, distance );
+	EXPECT_EQ(0, distanceFromPointToLine(line, point));
+}
+
+TEST(UtilsTestSuite, distanceFromPointToLineBasicTestTwo)
+{
+	Point point{ 0, 0 };
+	Line line{ 1, 1, 4 };
+	EXPECT_EQ( roundf( sqrt(2) * 2 * 100 ) / 100, roundf( distanceFromPointToLine( line, point ) * 100 ) / 100 );
 }
