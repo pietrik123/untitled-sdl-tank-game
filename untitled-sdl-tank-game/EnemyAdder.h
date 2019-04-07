83e988a02c76d44a623b1dbe2d11aed687fae100
pietrik123 <piotrmierzwa042@gmail.com>
2019-04-07 09:56:24 +0200

    doxygen added, 'couts' removed

--- untitled-sdl-tank-game/EnemyAdder.h
+++ untitled-sdl-tank-game/EnemyAdder.h
@@ -9,13 +9,33 @@ class EnemyAdder
 {
 public:
     int maxNumOfEnemies;
+
+    /**
+     * enemies' counter
+     */
     unsigned int cnt;
+
+    /**
+     * \c Player object reference
+     */
     const Player& player;
+
+    /**
+     * \c Enemy object, which will be copied and added by \c EnemyAdder
+     */
     const Enemy& enemyTemplate;
     bool timerElapsed;
     int timerId;
 
+    /**
+     * \param paramEnemyTemplate \c Enemy object which should be added
+     * \param paramMaxNumOfEnemies
+     * \param paramPlayer reference to \c Player object
+     */
     EnemyAdder(const Enemy& paramEnemyTemplate, int paramMaxNumOfEnemies, const Player& paramPlayer);
 
+    /**
+     * this method should be executed in game main loop
+     */
     void run(std::vector<Enemy>& enemies);
 };
