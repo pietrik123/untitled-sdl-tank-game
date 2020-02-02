da71ef7b62d7412e124f9b2c4b6167922866497b
dante <dante7867@gmail.com>
2020-02-02 12:28:07 +0100

    seed of class for reading attributes from .svg files

--- xml-parser-test/xml-parser-test/xml-parser-test.cpp
+++ xml-parser-test/xml-parser-test/xml-parser-test.cpp
@@ -6,26 +6,60 @@
 #include "rapidxml_print.hpp"
 #include "rapidxml_utils.hpp"
 
+class RapidXmlWrapper
+{
+public:
+    rapidxml::file<> _xmlFile;
+    rapidxml::xml_document<> _doc;
+
+    RapidXmlWrapper(const char* fileName)
+        :_xmlFile{ fileName }
+    {
+        _doc.parse<0>(_xmlFile.data());
+    }
+    void printNodeAttributes(const rapidxml::xml_node<>* node);
+    void printNodes();
+    void printAttrubutesOfNode(const char* name);
+};
+
+void RapidXmlWrapper::printNodeAttributes(const rapidxml::xml_node<>* node)
+{
+    std::cout << "Printing attributes for \" " << node->name() << "\"\n";
+    std::cout << "Node " << node->name() << " has value " << node->value() << "\n";
+    for (rapidxml::xml_attribute<>* attr = node->first_attribute();
+        attr; attr = attr->next_attribute())
+    {
+        std::cout << "Attribute = " << attr->name() << " ";
+        std::cout << ", value = " << attr->value() << "\n";
+    }
+}
+
+void RapidXmlWrapper::printNodes()
+{
+    for (rapidxml::xml_node<>* node = _doc.first_node()->first_node(); node; node = node->next_sibling())
+    {
+        std::cout << node->name() << std::endl;
+    }
+}
+void RapidXmlWrapper::printAttrubutesOfNode(const char* name)
+{
+    for (rapidxml::xml_node<>* node = _doc.first_node()->first_node(); node; node = node->next_sibling())
+    {
+        if (*(node->name()) == *name)
+        {
+            printNodeAttributes(node);
+        }
+    }
+}
+
+
 
 int main()
 {
-    using namespace rapidxml;
-    file<> xmlFile("lake_col.svg"); // Default template is char
-    xml_document<> doc;
-    doc.parse<0>(xmlFile.data());
-
-    // Printing.
-    //std::cout << doc << std::endl;
-
-    std::cout << "Name of my first node is: " << doc.first_node()->name() << "\n";
-    xml_node<>* node = doc.first_node("svg");
-    std::cout << "Node foobar has value " << node->value() << "\n";
-    for (xml_attribute<>* attr = node->first_attribute();
-        attr; attr = attr->next_attribute())
-    {
-        std::cout << "Node foobar has attribute " << attr->name() << " ";
-        std::cout << "with value " << attr->value() << "\n";
-    }
+    std::cout << "creating my wrapper..." << std::endl;
+    RapidXmlWrapper xmlwrapper("lake_col.svg");
+    auto firstNode = xmlwrapper._doc.first_node();
+    xmlwrapper.printAttrubutesOfNode("circle");
 }
 
 
